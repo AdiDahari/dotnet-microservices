3dde2091cba8a5080a9b89c4388d83e6e6315737
AdiDahari <dahariadi16@gmail.com>
2023-03-07 17:35:44 +0200

    initial commit - monorepo

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Database . . .");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "DotNET", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Not Seeding. Database is already populated");
+            }
+
+        }
+    }
+}

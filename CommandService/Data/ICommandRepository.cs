89c11fb28ca3870162cad56a3e70a5cbf5cd18f8
AdiDahari <dahariadi16@gmail.com>
2023-03-08 19:27:41 +0200

    created commands models and repository

---
+++ CommandService/Data/ICommandRepository.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandService.Models;
+
+namespace CommandService.Data
+{
+    public interface ICommandRepository
+    {
+        bool SaveChanges();
+
+        // Platform Related
+        IEnumerable<Platform> getAllPlatforms();
+        void CreatePlatform(Platform platform);
+        bool PlatformExists(int platformId);
+
+        // Commands related
+        IEnumerable<Command> getCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+
+    }
+}
